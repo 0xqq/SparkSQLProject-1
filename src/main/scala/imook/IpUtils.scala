5de33748f590fca695c2c7586b368a2d1d26f7cf
shujuelin <503989543@qq.com>
2018-06-05 17:42:50 +0800

    SparkSQL

---
+++ src/main/scala/imook/IpUtils.scala
@@ -0,0 +1,21 @@
+package imook
+
+import com.ggstar.util.ip.IpHelper
+
+/**
+  * ip解析工具类  把ip转化为城市
+  */
+object IpUtils {
+
+  def getCity(ip: String) = {
+
+    IpHelper.findRegionByIp(ip)
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    println(getCity("58.30.15.255"))
+
+  }
+
+}
